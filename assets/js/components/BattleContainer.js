29471951d601318431ca3da3f7024e688c08a609
Zt-freak <bnkho98@hotmail.com>
2020-07-26 19:34:40 +0200

    separated component into two separate components

--- assets/js/components/BattleContainer.js
+++ assets/js/components/BattleContainer.js
@@ -1,17 +1,18 @@
 import React from 'react'
+import PlayersContainer from './PlayersContainer'
 
 class BattleContainer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       players: [],
-      currentTurn: null
+      currentTurn: null,
+      messages: ['battle initialised']
     }
   }
 
   initialise () {
     for (let i = 0; i < this.props.players.length; i++) {
-      console.log(this.props.players[i])
       fetch("/battle/api/get/player", {
         method: 'POST',
         mode: 'cors',
@@ -45,11 +46,39 @@ class BattleContainer extends React.Component {
                 currentTurn: this.state.players[0].id
               })
             }
-
-            console.log (this.state)
-            this.forceUpdate()
           }
         )
+    }
+  }
+
+  nextTurn () {
+    // Get the player from the last turn
+    const lastPlayer = this.state.players.find(x => x.id === this.state.currentTurn)
+    
+    // Get the index from the last player
+    let lastPlayerIndex = this.state.players.indexOf(lastPlayer)
+    
+    // Set the next player
+    let NextPlayerFound = false
+    for (;NextPlayerFound == false;) {
+      lastPlayerIndex++
+      // If the player is the last one in this.state.players, go back to index 0
+      if (lastPlayerIndex >= this.state.players.length - 1) {
+        lastPlayerIndex = 0
+      }
+
+      // If the player has 0 HP, skip
+      if (this.state.players[lastPlayerIndex].currenthitpoints <= 0) {
+        lastPlayerIndex++
+      }
+      // Else set the new player
+      else {
+        this.setState({
+          currentTurn: this.state.players[lastPlayerIndex].id
+        })
+        NextPlayerFound = true
+        this.state.messages.push(this.state.players[lastPlayerIndex]+'\'s turn')
+      }
     }
   }
 
@@ -64,53 +93,11 @@ class BattleContainer extends React.Component {
   }
 
   render () {
-    // Check if players are loaded in
-    if (this.state.players.length > 0) {
-
-      let playerContainers = ''
-      let playerContainerClasses = 'playercontainer'
-      for (let i = 0; i < this.state.players.length; i++) {
-
-        playerContainerClasses = 'playercontainer'
-        if (this.state.currentTurn == this.state.players[i].id) {
-          playerContainerClasses = playerContainerClasses + ' playercontainer--current'
-        }
-        if (this.state.players[i].currenthitpoints < (this.state.players[i].hitpoints/10)) {
-          playerContainerClasses = playerContainerClasses + ' playercontainer--damaged'
-        }
-
-        playerContainers = (
-        <>
-          {playerContainers}
-          <div className={playerContainerClasses} id={this.state.players[i].id}>
-            <div className="playernamecontainer">
-              <span className="name">{this.state.players[i].username}</span>
-              <span className="level">lvl. {this.state.players[i].level}</span>
-            </div>
-            <div className="hitpointbar">
-              <div className="hitpoints" style={{height: "100%", width: "calc(100% / " + this.state.players[i].hitpoints +" * " + this.state.players[i].currenthitpoints + ")"}}></div>
-            </div>
-            <div>HP: {this.state.players[i].currenthitpoints}/{this.state.players[i].hitpoints}</div>
-          </div>
-        </>
-        )
-      }
-
-      return (
-        <div className="battlecontainer">
-          <div className="players">
-            {playerContainers}
-          </div>
-        </div>
-      )
-    }
-    else {
-      return (
-        <div className="battlecontainer">
-          initialising battle... Please wait. :)
-        </div>
-      )
-    }
+    return (
+      <div className="battlecontainer">
+        <PlayersContainer players={this.state.players} currentTurn={this.state.currentTurn}/>
+      </div>
+    )
   }
 }
 
